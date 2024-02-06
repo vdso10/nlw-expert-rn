bc94932141a036509d553bab9e92c6f2a3ba23fa
devvagner <vaguinhobom@gmail.com>
2024-02-06 14:13:56 -0300

    Inciando as configuração do aplicativo, como fontes e cores

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+
+
+export default function Home(){
+    return(
+        <View className='flex-1 pt-8'>
+            
+        </View>
+    )
+  
+
+}
+
+

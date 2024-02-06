62acfb27a7b4edb9b25988c69c605596895ec19a
devvagner <vaguinhobom@gmail.com>
2024-02-06 16:04:25 -0300

    Atualizando as informações do category-button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,19 @@
 import { View, Text } from "react-native";
 import { Header } from "../components/Header";
+import { CategoryButtom } from "../components/Category-Button";
 
 
 export default function Home(){
     return(
         <View className='flex-1 pt-8'>
             <Header title="Faça seu pedido" cartQuantityItems={0}/>
+
+            <View className=" flex-row gap-4">
+                <CategoryButtom title="Lanche do dia"/>
+                <CategoryButtom title="Lanche do dia" isSelected/>
+                <CategoryButtom title="Lanche do dia"/>
+
+            </View>
             
         </View>
     )
--- src/components/Category-Button.tsx
+++ src/components/Category-Button.tsx
@@ -1,18 +1,20 @@
 import { Text, Pressable, PressableProps } from 'react-native'
+import clsx from 'clsx'
 
 
 
-type CategoryProps = {
+type CategoryProps = PressableProps & {
     title: string
-    isSelected: boolean
+    isSelected?: boolean
 }
 
 
 
-export function CategoryButtom({title, isSelected}: CategoryProps){
+export function CategoryButtom({title, isSelected, ...rest}: CategoryProps){
     return(
-        <Pressable>
-            <Text>{title}</Text>
+        <Pressable className={clsx('bg-slate-800 px-4 justify-center rounded-md h-10', 
+        isSelected && 'border-2 border-lime-3')}>
+            <Text className='text-slate-100 font-subtitle text-sm'>{title}</Text>
         </Pressable>
 
     )

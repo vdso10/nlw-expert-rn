6e0f92c4e61bf3d9be1032acedc1111cbbc8905d
devvagner <vaguinhobom@gmail.com>
2024-02-07 14:57:50 -0300

    Finalizando o header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,42 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, FlatList } from "react-native";
+
+import { CATEGORIES } from "../utils/data/products";
+
 import { Header } from "../components/Header";
 import { CategoryButtom } from "../components/Category-Button";
 
 
 export default function Home(){
+
+    const [category, setCategory] = useState(CATEGORIES[0])
+
+    function handleCategorySelect(selectedCategory: string){
+        setCategory(selectedCategory)
+    }
+
+
     return(
         <View className='flex-1 pt-8'>
             <Header title="Faça seu pedido" cartQuantityItems={0}/>
 
-            <View className=" flex-row gap-4">
-                <CategoryButtom title="Lanche do dia"/>
-                <CategoryButtom title="Lanche do dia" isSelected/>
-                <CategoryButtom title="Lanche do dia"/>
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item)=>item}
+                renderItem={({item}) => (
+                <CategoryButtom 
+                title={item}
+                isSelected={item === category}
+                onPress={()=> handleCategorySelect(item)} 
+                />
+                )}
+                horizontal
+                className="max-h-10 mt-5"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
 
-            </View>
+                
+            />
             
         </View>
     )
--- src/components/Category-Button.tsx
+++ src/components/Category-Button.tsx
@@ -1,5 +1,5 @@
 import { Text, Pressable, PressableProps } from 'react-native'
-import clsx from 'clsx'
+import {clsx} from 'clsx'
 
 
 
@@ -12,8 +12,11 @@ type CategoryProps = PressableProps & {
 
 export function CategoryButtom({title, isSelected, ...rest}: CategoryProps){
     return(
-        <Pressable className={clsx('bg-slate-800 px-4 justify-center rounded-md h-10', 
-        isSelected && 'border-2 border-lime-3')}>
+        <Pressable 
+            className={clsx('bg-slate-800 px-4 justify-center rounded-md h-10', 
+            isSelected && 'border-2 border-lime-300')}
+            {...rest}
+            >
             <Text className='text-slate-100 font-subtitle text-sm'>{title}</Text>
         </Pressable>
 
